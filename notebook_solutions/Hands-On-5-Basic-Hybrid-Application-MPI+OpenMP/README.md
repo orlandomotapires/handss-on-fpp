cfefdddbadedae08b027688d40cf76548e5b6a7b
orlandomotapires <orlandomota2002@gmail.com>
2023-10-11 09:01:13 -0300

    first one ready

--- notebook_solutions/Hands-On-5-Basic-Hybrid-Application-MPI+OpenMP/README.md
+++
@@ -1,3 +0,0 @@
-# Hands-On 5: Basic Hybrid Application MPI+OpenMP
-
-*  Matrix Multiple MPI+OpenMP
--- notebook_solutions/Hands-On-5-Basic-Hybrid-Application-MPI+OpenMP/material/mm-openmp.c
+++ notebook_solutions/Hands-On-5-Basic-Hybrid-Application-MPI+OpenMP/material/mm-openmp.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <omp.h>
 
+void writeResultsToFile(int size, double time_spend, int num_threads) {
+    FILE *output_file = fopen("./output/mm_openMP.txt", "a");
+    if (output_file == NULL) {
+        perror("Erro ao abrir o arquivo de saída");
+        return;
+    } 
+
+    fprintf(output_file, "%d %lf %d ", size, time_spend, num_threads);
+    if(size == 1000) fprintf(output_file, "\n");
+
+    fclose(output_file);
+}
+
 void initializeMatrix(int *matrix, int size)
 {
   for (int i = 0; i < size; i++)
@@ -36,12 +49,18 @@ int main (int argc, char **argv)
 
  t1 = omp_get_wtime();
 
- #pragma omp parallel for private(i, j, k)
-   for(i = 0; i < size; i++)
-    for(j = 0; j < size; j++)
-      for(k = 0; k < size; k++)
-        C[i * size + j] += A[i * size + k] * B[k * size + j];
+  int num_threads = 0;
 
+ #pragma omp parallel for private(i, j, k)
+  for(i = 0; i < size; i++){
+    for(j = 0; j < size; j++){
+      for(k = 0; k < size; k++){
+        C[i * size + j] += A[i * size + k] * B[k * size + j];
+        if(i==0 && j==0 && k == 0)num_threads = omp_get_num_threads();
+      }     
+    }  
+  }
+ 
  t2 = omp_get_wtime();
 
  printf("%d\t%f\n",size, t2-t1);
@@ -49,6 +68,8 @@ int main (int argc, char **argv)
 // printMatrix(A,size);
 // printMatrix(B,size);
 // printMatrix(C,size);
+
+writeResultsToFile(size, t2-t1, num_threads);
 
  return 0;
 
