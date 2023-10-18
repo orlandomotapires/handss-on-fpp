a5b45f5cd03e588c451763e6927de53a892012f0
orlandomotapires <orlandomota2002@gmail.com>
2023-10-18 09:55:43 -0300

    final 5 e 6

--- notebook_solutions/Hands-On-5-Basic-Hybrid-Application-MPI+OpenMP/bash/mm-mpi_openmp.sh
+++ notebook_solutions/Hands-On-5-Basic-Hybrid-Application-MPI+OpenMP/bash/mm-mpi_openmp.sh
@@ -2,11 +2,17 @@
 
 for ((i=1; i<=32; i*=2)) # num_threads
 do
-    for ((j=1; j<=8; j*=2)) # num_processors
-    do
+    for ((j=1; j<=4; j*=2)) # num_processors
+    do    
+
+        if [ "$i" == 1 ] && [ "$j" == 2 ]; then 
+            break
+        fi
+
         for ((k=200; k<=1000; k+=200))
         do
-            mpirun -np "$j" ./material/mm-mpi_openmp  "$k" "$i" 
+            mpirun -np "$j" ./material/mm-mpi_openmp "$k" "$i"
         done
+        
     done
 done
